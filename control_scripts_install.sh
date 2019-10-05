d03ece596057beebbd7e39cb8010a9c7d155c105
James Livingstone <jlivin25@live.co.uk>
2019-10-05 09:15:24 +0100

    Corrected syntax errors. Added variable

--- control_scripts_install.sh
+++ control_scripts_install.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 #
 UDEV_LOC="/etc/udev/rules.d/"
-if (whoami != root)
+SYSD_LOC="/etc/systemd/system/"
+if [ whoami != root ];
   then echo "Please run as root"
    exit 1
-  else (do stuff)
+  else
    cd "$(dirname "$0")"
    chmod 644 *.rules
    cp *.rules $UDEV_LOC
    udevadm control --reload-rules
+   chmod 444 *.service
+   cp *.service $SYSD_LOC
 fi
-
 exit 0
