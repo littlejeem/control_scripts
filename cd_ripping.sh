03267c759a1d3f94ffc9ce43adf238959a19c66b
James Livingstone <jlivin25@live.co.uk>
2019-10-05 22:31:58 +0100

    inspection of variables

--- cd_ripping.sh
+++ cd_ripping.sh
@@ -7,27 +7,28 @@
 ### (1) abcde                                                                                          ###
 ### (2) 82-AutoCDInsert.rules which triggers                                                           ###
 ### (3) cd_ripping.service  which triggers this script if Audio CD is detected                         ###
+### (4) https://github.com/littlejeem/abcde_configs.git to provide abcde configs                       ###
 ##########################################################################################################
 #
 #
 #####################
-### SET VARIABLES ###
+### SET VARIABLES ### <-----------------move to a config file and read in?
 #####################
 locknamelong=`basename "$0"`                      # imports the name of this script
 lockname=${locknamelong::-3}                      # reduces the name to remove .sh
 logfolder=/home/jlivin25/bin/myscripts/scriptlogs # Where the logs are kept
 lognameCDRipping=$lockname.log                    # Uses the script name to create the log
-INALACPATH=/home/jlivin25/Music/m4a               # m4a
-INFLACPATH=/home/jlivin25/Music/flac              #
-OUTALACPATH=/media/Data_1/Music/correct/Albums    #
-OUTFLACPATH=/media/Data_1/Music/FLAC_Backups      #
+INALACPATH=/home/jlivin25/Music/m4a               # - still in use?
+INFLACPATH=/home/jlivin25/Music/flac              # - still in use?
+OUTALACPATH=/media/Data_1/Music/correct/Albums    # - still in use?
+OUTFLACPATH=/media/Data_1/Music/FLAC_Backups      # - still in use?
 #
 #
 ########################
 ### DEFINE FUNCTIONS ###
 ########################
 ripcd_flac () {
-abcde -j `getconf _NPROCESSORS_ONLN` -N -c /home/jlivin25/bin/myscripts/abcde_configs/abcde_flac.conf
+abcde -j `getconf _NPROCESSORS_ONLN` -N -c /home/jlivin25/bin/myscripts/abcde_configs/abcde_flac.conf # <----------SWITCH TO VARIABLE IN CONFIG?
 }
 #
 #
@@ -38,12 +39,11 @@ echo "----------------------------------------------------" >> $logfolder/$logna
 echo "`date +%d/%m/%Y` - `date +%H:%M:%S` - Script Started" >> $logfolder/$lognameCDRipping
 #
 echo "`date +%d/%m/%Y` - `date +%H:%M:%S` - Stage 1 - FLAC Ripping Started" >> $logfolder/$lognameCDRipping
-cd /home/jlivin25/Music/RipTransfers
+cd /home/jlivin25/Music/RipTransfers # <----------SWITCH TO VARIABLE IN CONFIG?
 ripcd_flac
 echo "`date +%d/%m/%Y` - `date +%H:%M:%S` - Stage 2 - FLAC Ripping Completed" >> $logfolder/$lognameCDRipping
-#
 echo "`date +%d/%m/%Y` - `date +%H:%M:%S` - Stage 3 - Syncing Rip to Library" >> $logfolder/$lognameCDRipping
-#sudo -u jlivin25 /home/jlivin25/bin/myscripts/MusicSync.sh
+sudo -u jlivin25 /home/jlivin25/bin/myscripts/MusicSync.sh # <----------SWITCH TO VARIABLE IN CONFIG?
 eject
 echo "`date +%d/%m/%Y` - `date +%H:%M:%S` - Stage 4 - Complete - CD Ejected, End of Script" >> $logfolder/$lognameCDRipping
 #
