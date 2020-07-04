c3c9f9d272d77b18c6ec615118c177a5714c5a71
James Livingstone <jlivin25@live.co.uk>
2020-07-04 17:15:16 +0100

    changed rip location

--- ripping_scripts/bluray_ripping2.sh
+++ ripping_scripts/bluray_ripping2.sh
@@ -2,7 +2,7 @@
 source_drive="disc:0"
 dev_drive="/dev/sr0"
 working_dir="/home/jlivin25"
-rip_dest="Rips"
+rip_dest="Rips/bluray"
 bluray_name=$(blkid -o value -s LABEL "$source_drive")
 bluray_name=${bluray_name// /_}
 makemkvcon backup "$source_drive" "$working_dir"/"$rip_dest"/"$bluray_name"
