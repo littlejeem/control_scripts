d0d7da827949d99b760bd4cc6f486f6344bcf115
James Livingstone <jlivin25@live.co.uk>
2020-07-04 09:50:43 +0100

    addition of further variables, first rip test

--- ripping_scripts/bluray_ripping2.sh
+++ ripping_scripts/bluray_ripping2.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 source_drive="/dev/sr0"
+working_dir="/home/jlivin25"
+rip_dest="Rips"
 bluray_name=$(blkid -o value -s LABEL "$source_drive")
 bluray_name=${bluray_name// /_}
+makemkvcon backup $source_drive "$working_dir"/"$rip_dest"/"$bluray_name"
