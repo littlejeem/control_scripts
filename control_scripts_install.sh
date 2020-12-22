8ff8922bc31e0079fdbdda7ce0f0fc24a4b961e4
James Livingstone <littlejeem@protonmail.com>
2020-12-22 22:45:22 +0000

    added some error checking, added install of abcde

--- control_scripts_install.sh
+++ control_scripts_install.sh
@@ -1,20 +1,93 @@
 #!/bin/bash
 #
-####################################
-### THS NEEDS WORK FOR VERSION 2 ###
-####################################
-
-UDEV_LOC="/etc/udev/rules.d/"
-SYSD_LOC="/etc/systemd/system/"
+#
+install_user=jlivin25
+udev_loc="/etc/udev/rules.d/"
+sysd_loc="/etc/systemd/system/"
+#
+#
+#+----------------------+
+#+---"Check for Root"---+
+#+----------------------+
 if [[ $EUID -ne 0 ]]; then
   echo "This script must be run as root"
   exit 1
+fi
+#
+#
+#+-------------------------+
+#+---"Set up UDEV rules"---+ <---(symlink?)
+#+-------------------------+
+cp -r udev_rules/. $udev_loc
+if [[ $? -ne 0 ]]; then
+  log_err "copying UDEV rules failed"
+  exit 1
 else
-  chmod -R 644 udev_rules
-  cp -r udev_rules/. $UDEV_LOC
-  udevadm control --reload-rules
-  chmod -R 444 systemd_files
-  cp -r systemd_files/. $SYSD_LOC
-  systemctl daemon-reload
+  log "UDEV rules copied"
+fi
+#modify permissions
+chmod -R 644 udev_rules
+if [[ $? -ne 0 ]]; then
+  log_err "changing mode of UDEV files failed"
+  exit 1
+else
+  log "changing mode of UDEV files succeded"
+fi
+#reload udev rules
+udevadm control --reload
+#
+if [[ $? -ne 0 ]]; then
+  log_err "Reloading UDEV rules failed"
+  exit 1
+else
+  log "Reloading UDEV rules succeded"
+fi
+#
+#
+#+-------------------------------+
+#+---"Set up SYSTEMD services"---+ <---(symlink?)
+#+-------------------------------+
+cp -r systemd_files/. $sysd_loc
+if [[ $? -ne 0 ]]; then
+  log_err "copying services to systemd failed"
+  exit 1
+else
+  log "Services copied to systemd"
+fi
+#modify permissions
+chmod -R 444 systemd_files
+if [[ $? -ne 0 ]]; then
+  log_err "changing mode of service files failed"
+  exit 1
+else
+  log "changing mode of service files succeded"
+fi
+#reload udev rules
+systemctl daemon-reload
+#
+if [[ $? -ne 0 ]]; then
+  log_err "Reloading UDEV rules failed"
+  exit 1
+else
+  log "Reloading UDEV rules succeded"
+fi
+#
+#
+#+-------------------------------+
+#+---"Install ABCDE cd ripper"---+
+#+-------------------------------+
+if ! command -v abcde &> /dev/null
+then
+  log_err "abcde could not be found, script won't function wihout it, attempting install"
+  apt update && apt install abcde
+  if ! command -v abcde &> /dev/null
+  then
+    log_err "abcde install failed, scripts won't function wihout it, exiting"
+    exit 1
+  else
+    log "abcde now installed, continuing"
+  fi
+else
+    log "abcde command located, continuing"
 fi
 exit 0
